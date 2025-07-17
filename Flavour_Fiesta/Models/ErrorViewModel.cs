3c2def3adb08c54e78ab6f6b6dcd00901cdb6f68
5wethaa <swethamahesrani@gmail.com>
2025-07-17 13:17:11 +0530

    update media code

--- Flavour_Fiesta/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Flavour_Fiesta.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
